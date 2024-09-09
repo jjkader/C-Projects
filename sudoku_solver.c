f64411fe9ec78aa82127262a9906d9d2512b752d
jjkader <86396198+jjkader@users.noreply.github.com>
2024-09-09 14:37:05 -0400

    Update sudoku_solver.c

--- sudoku_solver.c
+++ sudoku_solver.c
@@ -1,36 +1,7 @@
-/**
- * Exercise 11 - Sudoku
- *
- * Please read the comments below carefully, they describe your task in detail.
- *
- * Any clarifications and corrections will be posted to Piazza so please keep an
- * eye on the forum!
- *
- * Unauthorized distribution or posting is strictly prohibited. You must seek
- * approval from course staff before uploading and sharing with others.
- *
- * Starter code:  (Updated by) Charles Xu, 2024
- *             (Originally by) Mustafa Quraish, 2020
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-// ===========================================================================
-// DO NOT CHANGE ANY 'ifndef' AND 'endif' MACROS.
-// These macros help ensure your parts of your code does not conflict with the
-// automarker. Changing them will result in your code not compiling and you
-// receiving a 0 for the entire assignment.
-// ===========================================================================
-#ifndef __testing__
-
-/**
- * Given a 9x9 Sudoku grid, this function prints out the grid in a format
- * that is easy to view, aiding in debugging or visualization.
- *
- * @param sudoku The 9x9 Sudoku grid to be printed.
- */
 void print_sudoku(int sudoku[9][9]) {
     printf("The Sudoku contains:\n");
     printf("+-------+-------+-------+\n");
@@ -44,19 +15,6 @@ void print_sudoku(int sudoku[9][9]) {
         if (j % 3 == 2) printf("+-------+-------+-------+\n");
     }
 }
-
-#endif
-
-/**
- * This function checks if the current Sudoku grid (not necessarily complete)
- * is valid. A Sudoku grid is considered valid if, ignoring the 0s, each rows,
- * column, and 3x3 subgrid does not repeat any digits from 1 to 9.
- *
- * @note An incomplete Sudoku grid may be valid if the criteria above is met.
- *
- * @param sudoku The 9x9 Sudoku grid to be checked.
- * @return 1 if the Sudoku is valid, 0 if it's not.
- */
 void initialize_array(int arr[]){
     for (int i = 0; i < 9; ++i){
         arr[i] = 0;
@@ -126,19 +84,6 @@ int is_valid_sudoku(int sudoku[9][9]) {
     }
     return 1;
 }
-
-/**
- * Given a (possibly partially) completed Sudoku grid, this function attempts
- * to solve the Sudoku. The `sudoku` array is changed to contain the solution
- * if one exists. None of the digits in the input grid should be changed.
- *
- * @note: Empty spots in the grid are denoted with 0s.
- *
- * @param sudoku The 9x9 Sudoku grid to be solved.
- * @return 1 if the Sudoku was solved successfully and the `sudoku` array is
- * updated, 0 if it's not possible to solve the Sudoku, and the input `sudoku`
- * array remains unchanged in this case.
- */
 int check_completion(int sudoku[9][9], int *row, int *col){
     for (int i = 0; i < 9; ++i){
         for (int j = 0; j < 9; ++j){
@@ -169,14 +114,6 @@ int solve_sudoku(int sudoku[9][9]){
     return 0;
 }
 
-// ===========================================================================
-// DO NOT CHANGE ANY 'ifndef' AND 'endif' MACROS.
-// These macros help ensure your parts of your code does not conflict with the
-// automarker. Changing them will result in your code not compiling and you
-// receiving a 0 for the entire assignment.
-// ===========================================================================
-#ifndef __testing__
-
 int main() {
     int sudoku_grid[9][9] = {
         {5, 3, 0, 0, 7, 0, 0, 0, 0},
@@ -195,11 +132,8 @@ int main() {
     int solved = solve_sudoku(sudoku_grid);
 
     printf("\nsolve_sudoku() return value: %d\n\n", solved);
-
-    /* Look at the handout to see the solution for this! */
+    
     print_sudoku(sudoku_grid);
 
     return 0;
 }
-
-#endif
